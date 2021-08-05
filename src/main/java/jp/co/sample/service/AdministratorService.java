1c0c800fbb091ff7d71dfa316ef87e84e70671d6
tnakamura0314 <reggae.a0215@gmail.com>
2021-08-05 14:34:39 +0900

    AdministratorServiseクラスにinsertメソッドの追加

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 /**
@@ -18,5 +19,17 @@ public class AdministratorService {
 	
 	@Autowired
 	private AdministratorRepository administratorRepository;
+	
+	/**
+	 * 管理者情報を登録するメソッド.<br>
+	 * administratorRepositoryのinsert()メソッドを呼ぶ処理
+	 * 
+	 * @param administrator　
+	 */
+	public void insert(Administrator administrator) {
+		
+		administratorRepository.insert(administrator);
+		
+	}
 
 }
