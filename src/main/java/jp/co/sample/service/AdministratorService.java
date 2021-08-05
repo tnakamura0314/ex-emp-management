a1c44adb2e1aba93b0ed685190e38a66ca6061da
tnakamura0314 <reggae.a0215@gmail.com>
2021-08-05 13:41:47 +0900

    AdministratorService/InsertAdministratorFormの作成

---
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -0,0 +1,22 @@
+package jp.co.sample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.repository.AdministratorRepository;
+
+/**
+ * Administratorのサービスクラス.
+ * 
+ * @author nakamuratomoya
+ *
+ */
+@Service
+@Transactional
+public class AdministratorService {
+	
+	@Autowired
+	private AdministratorRepository administratorRepository;
+
+}
