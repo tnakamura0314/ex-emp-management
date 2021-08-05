84c1ddeeed20f7b719fea14e6bfface930d9bb15
tnakamura0314 <reggae.a0215@gmail.com>
2021-08-05 14:43:15 +0900

    AdministratorControllerにinsertメソッドの追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,12 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -17,10 +19,10 @@ import jp.co.sample.service.AdministratorService;
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
-	
+
 	@Autowired
 	private AdministratorService administratorService;
-	
+
 	/**
 	 * InsertAdministratorFormオブジェクトを自動でModelオブジェクトに格納するメソッド.
 	 * 
@@ -30,7 +32,7 @@ public class AdministratorController {
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
 		return new InsertAdministratorForm();
 	}
-	
+
 	/**
 	 * administrator/insert.htmlにフォワードするメソッド.
 	 * 
@@ -40,6 +42,22 @@ public class AdministratorController {
 	public String toInsert() {
 		return "administrator/insert.html";
 	}
-	
+
+	/**
+	 * 管理者情報を登録するinsertメソッド.
+	 * 
+	 * @param form 管理者情報登録時に使用するフォーム
+	 * @return ログイン画面にリダイレクト
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+
+		administratorService.insert(administrator);
+
+		return "redirect:/";
+	}
 
 }
