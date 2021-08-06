8421af232f55dfe3052b1358a5967da2d1089e76
tnakamura0314 <reggae.a0215@gmail.com>
2021-08-06 10:57:27 +0900

    detail.htmlの作成

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -23,6 +25,11 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
 	
 	/**
 	 * 従業員一覧を取得.
@@ -37,7 +44,27 @@ public class EmployeeController {
 		
 		model.addAttribute("employeeList", employeeList);
 		
-		return "employee/list";
+		return "/employee/list";
+		
+	}
+	
+	/**
+	 * 従業員詳細情報を取得.
+	 * 
+	 * @param id　従業員ID
+	 * @param model　requestスコープ
+	 * @return 従業員詳細情報
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+
+		int id2 = Integer.parseInt(id);
+		
+		Employee employee = employeeService.showDetail(id2);
+		
+		model.addAttribute("employee", employee);
+		
+		return "/employee/detail";
 		
 	}
 	
